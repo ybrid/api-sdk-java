347548033d69c1aa727d76710123fc0dfcce4bed
Philipp Schafft <philipp.schafft@nacamar.de>
2019-09-23 10:45:55 +0000

    Cleanup: Optimized imports

--- src/main/java/io/ybrid/client/control/StreamSession.java
+++ src/main/java/io/ybrid/client/control/StreamSession.java
@@ -18,13 +18,11 @@ package io.ybrid.client.control;
 
 import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class StreamSession implements Connectable {
     private boolean connected = false;
