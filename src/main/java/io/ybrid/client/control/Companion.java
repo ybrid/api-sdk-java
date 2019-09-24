3c2a21bf3b3f6869a52ce52bbd84bf0b87d24ae2
Philipp Schafft <philipp.schafft@nacamar.de>
2019-09-24 14:25:55 +0000

    Feature: Added classes for Items and Companions

---
+++ src/main/java/io/ybrid/client/control/Companion.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2019 nacamar GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.ybrid.client.control;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Companion {
+    private String alternativeText;
+    private int height;
+    private int width;
+    private int sequenceNumber;
+    private URL staticResource;
+    private URL onClick;
+    private URL onView;
+
+    public Companion(JSONObject json) throws MalformedURLException {
+        alternativeText = json.getString("altText");
+        height = json.getInt("height");
+        width = json.getInt("width");
+        sequenceNumber = json.getInt("sequenceNumber");
+        staticResource = new URL(json.getString("staticResourceURL"));
+        onClick = new URL(json.getString("onClickThroughURL"));
+        onView = new URL(json.getString("onCreativeViewURL"));
+    }
+
+    private void ping(URL url) throws IOException {
+        URLConnection connection = url.openConnection();
+        connection.setDoInput(false);
+        connection.setDoOutput(false);
+        connection.connect();
+    }
+
+    public void onClick() throws IOException {
+        ping(onClick);
+    }
+
+    public void onView() throws IOException {
+        ping(onView);
+    }
+
+    public String getAlternativeText() {
+        return alternativeText;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    public URL getStaticResource() {
+        return staticResource;
+    }
+
+    public URL getOnClick() {
+        return onClick;
+    }
+
+    public URL getOnView() {
+        return onView;
+    }
+}
---
+++ src/main/java/io/ybrid/client/control/Item.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2019 nacamar GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.ybrid.client.control;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.util.*;
+
+public class Item {
+    public static final String METADATA_TITLE = "title";
+    public static final String METADATA_ARTIST = "artist";
+    public static final String METADATA_DESCRIPTION = "description";
+
+    private static final String[] metadataList = {METADATA_ARTIST, METADATA_DESCRIPTION, METADATA_TITLE};
+
+    public enum Type {
+        ADVERTISEMENT, COMEDY, JINGLE, MUSIC, NEWS, VOICE;
+    }
+
+    private String identifier;
+    private HashMap<String, String> metadata = new HashMap<>();
+    private Type type;
+    private long duration;
+    private ArrayList<Companion> companions = new ArrayList<>();
+
+    public Item(JSONObject json) throws MalformedURLException {
+        identifier = json.getString("id");
+        JSONArray array;
+
+        for (String key : metadataList) {
+            String value = json.getString(key);
+            if (value != null && !value.isEmpty())
+                metadata.put(key, value);
+        }
+
+        type = Type.valueOf(json.getString("type"));
+
+        duration = json.getLong("durationMillis");
+
+        array = json.getJSONArray("companions");
+        if (array != null) {
+            for (int i = 0; i < array.length(); i++) {
+                companions.add(new Companion(array.getJSONObject(i)));
+            }
+        }
+    }
+
+    public String getDisplayName() {
+        String artist = metadata.get(METADATA_ARTIST);
+        String title = metadata.get(METADATA_TITLE);
+
+        if (artist != null && title != null) {
+            return artist + " - " + title;
+        }
+
+        return title;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public Map<String, String> getMetadata() {
+        return Collections.unmodifiableMap(metadata);
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public List<Companion> getCompanions() {
+        return Collections.unmodifiableList(companions);
+    }
+}
