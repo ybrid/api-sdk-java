3bc491aa6afc93fcbfe24f8e61ea0fddf4ce05e1
Philipp Schafft <philipp.schafft@nacamar.de>
2020-08-27 08:39:12 +0000

    Feature: Added interface MessageBody

---
+++ src/main/java/io/ybrid/api/message/MessageBody.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2020 nacamar GmbH - Ybrid®, a Hybrid Dynamic Live Audio Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.ybrid.api.message;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * This interface is implemented by classes that hold information that can be send as the body of a message over the wire.
+ */
+public interface MessageBody {
+    /**
+     * Returns the full media type for this message body.
+     * This includes the MIME-Type as well as parameters like the charset.
+     * @return The full media type.
+     */
+    @NotNull String getMediaType();
+
+    /**
+     * Renders the message body into a array of bytes suitable for over-the-wire transmission.
+     * @return The result as a byte array.
+     */
+    @NotNull byte[] getBytes();
+}
