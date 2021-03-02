1b46bd354f9ce59afd5c9a5055a0bc65e5749a2e
Philipp Schafft <philipp.schafft@nacamar.de>
2021-03-02 14:51:40 +0000

    Fix: Use getDriver() not driver

--- src/main/java/io/ybrid/api/Session.java
+++ src/main/java/io/ybrid/api/Session.java
@@ -135,7 +135,7 @@ public final class Session implements Connectable, KnowsSubInfoState {
                 case CONNECT_INITIAL_TRANSPORT:
                 case RECONNECT_TRANSPORT: {
                     final @Nullable Map<String, Double> acceptedMediaFormats = playerControl != null ? playerControl.getAcceptedMediaFormats() : null;
-                    final @NotNull ServiceTransportDescription transportDescription = new ServiceURITransportDescription(new Source(SourceType.TRANSPORT), driver.getCurrentService(), metadataMixer, acceptedMediaFormats, mediaEndpoint.getAcceptedLanguages(), transaction, getActiveWorkarounds(), driver.getStreamURI(), null);
+                    final @NotNull ServiceTransportDescription transportDescription = new ServiceURITransportDescription(new Source(SourceType.TRANSPORT), getDriver().getCurrentService(), metadataMixer, acceptedMediaFormats, mediaEndpoint.getAcceptedLanguages(), transaction, getActiveWorkarounds(), getDriver().getStreamURI(), null);
 
                     Objects.requireNonNull(playerControl).connectTransport(transportDescription);
                     break;
