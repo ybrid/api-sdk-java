cbc983de34e879e64536608d21f882ac38d0b4cd
Philipp Schafft <philipp.schafft@nacamar.de>
2021-03-08 09:30:35 +0000

    Feature: Added parsing part of new URIBuilder

---
+++ src/main/java/io/ybrid/api/util/URIBuilder.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2021 nacamar GmbH - Ybrid®, a Hybrid Dynamic Live Audio Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.ybrid.api.util;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class URIBuilder {
+    private @NotNull String scheme;
+    private @Nullable String hostname;
+    private int port;
+    private @NotNull String path;
+    private @Nullable String query;
+    private @Nullable String fragment;
+
+    static private void assertValidScheme(@NotNull String uri, @NotNull String scheme) throws URISyntaxException {
+        if (!scheme.matches("^[a-zA-Z0-9.+-]+$"))
+            throw new URISyntaxException(uri, "Invalid scheme: " + scheme);
+    }
+
+    public URIBuilder(@NotNull String uri) throws URISyntaxException {
+        @NotNull String[] res;
+        @NotNull String rest;
+
+        System.out.println("uri = " + uri);
+
+        res = uri.split(":", 2);
+        if (res.length != 2)
+            throw new URISyntaxException(uri, "No scheme given");
+
+        assertValidScheme(uri, res[0]);
+        this.scheme = res[0];
+
+        if (!res[1].startsWith("//"))
+            throw new URISyntaxException(uri, "Invalid syntax");
+
+        res = res[1].substring(2).split("/");
+        if (res.length != 2)
+            throw new URISyntaxException(uri, "No path given");
+
+        rest = "/" + res[1];
+
+        if (res[0].isEmpty()) {
+            this.hostname = null;
+            this.port = 0;
+        } else {
+            if (res[0].startsWith("[")) {
+                res = res[0].substring(1).split("]:", 2);
+                if (res.length != 2) {
+                    res = new String[]{res[0].substring(1, res[0].length() - 1)};
+                }
+            } else {
+                res = res[0].split(":", 2);
+            }
+            try {
+                Utils.assertValidHostname(res[0]);
+            } catch (MalformedURLException e) {
+                throw new URISyntaxException(uri, "Invalid hostname: " + res[0]);
+            }
+            this.hostname = res[0];
+            if (res.length == 2) {
+                try {
+                    this.port = Integer.parseInt(res[1]);
+                } catch (Throwable e) {
+                    throw new URISyntaxException(uri, "Invalid port: " + res[1]);
+                }
+            } else {
+                this.port = 0;
+            }
+        }
+
+        res = rest.split("#", 2);
+        if (res.length == 2) {
+            this.fragment = res[1];
+        } else {
+            this.fragment = null;
+        }
+
+        res = res[0].split("\\?", 2);
+        if (res.length == 2) {
+            this.query = res[1];
+        } else {
+            this.query = null;
+        }
+        this.path = res[0];
+    }
+
+    private static @NotNull String javaURItoString(@NotNull URI uri) {
+        final @NotNull String javasIdea = uri.toASCIIString();
+        System.out.println("javasIdea = " + javasIdea);
+        if (javasIdea.startsWith("file:/") && !javasIdea.startsWith("file://"))
+            return "file://" + javasIdea.substring(5); // RFC's idea.
+        return javasIdea;
+    }
+    public URIBuilder(@NotNull URI uri) throws URISyntaxException {
+        this(javaURItoString(uri));
+    }
+
+    public URIBuilder(@NotNull URL url) throws URISyntaxException {
+        this(url.toURI());
+    }
+
+    @Contract(pure = true)
+    public @NotNull String getRawScheme() {
+        return scheme;
+    }
+
+    @Contract(pure = true)
+    public @Nullable String getRawHostname() {
+        return hostname;
+    }
+
+    @Contract(pure = true)
+    public int getRawPort() {
+        return port;
+    }
+
+    @Contract(pure = true)
+    public @NotNull String getRawPath() {
+        return path;
+    }
+
+    @Contract(pure = true)
+    public @Nullable String getRawQuery() {
+        return query;
+    }
+
+    @Contract(pure = true)
+    public @Nullable String getRawFragment() {
+        return fragment;
+    }
+
+    @Override
+    public String toString() {
+        return "URIBuilder{" +
+                "scheme='" + scheme + '\'' +
+                ", hostname='" + hostname + '\'' +
+                ", port=" + port +
+                ", path='" + path + '\'' +
+                ", query='" + query + '\'' +
+                ", fragment='" + fragment + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/io/ybrid/api/util/URIBuilderTest.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2021 nacamar GmbH - Ybrid®, a Hybrid Dynamic Live Audio Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.ybrid.api.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class URIBuilderTest {
+    private static abstract class Vector {
+        public final @NotNull String uri;
+
+        private Vector(@NotNull String uri) {
+            this.uri = uri;
+        }
+    }
+
+    private static final class InvalidVector extends Vector {
+        private InvalidVector(@NotNull String uri) {
+            super(uri);
+        }
+    }
+
+    private static final class ValidVector extends Vector {
+        public final @NotNull String scheme;
+        public final @Nullable String hostname;
+        public final int port;
+        public final @NotNull String path;
+        public final @Nullable String query;
+        public final @Nullable String fragment;
+
+        public ValidVector(@NotNull String uri, @NotNull String scheme, @Nullable String hostname, int port, @NotNull String path, @Nullable String query, @Nullable String fragment) {
+            super(uri);
+            this.scheme = scheme;
+            this.hostname = hostname;
+            this.port = port;
+            this.path = path;
+            this.query = query;
+            this.fragment = fragment;
+        }
+    }
+
+    private static interface Constructor {
+        @NotNull URIBuilder construct(@NotNull String uri) throws URISyntaxException, MalformedURLException;
+    }
+
+    private static final Vector[] vectors = new Vector[]{
+            new ValidVector("http://example.org/bla", "http", "example.org", 0, "/bla", null, null),
+            new ValidVector("http://example.org/bla?a=b", "http", "example.org", 0, "/bla", "a=b", null),
+            new ValidVector("http://example.org/bla#c", "http", "example.org", 0, "/bla", null, "c"),
+            new ValidVector("http://example.org/bla?a=b#c", "http", "example.org", 0, "/bla", "a=b", "c"),
+            new ValidVector("http://example.org:1234/bla?a=b#c", "http", "example.org", 1234, "/bla", "a=b", "c"),
+            new ValidVector("http://127.0.0.1:1234/bla?a=b#c", "http", "127.0.0.1", 1234, "/bla", "a=b", "c"),
+            new ValidVector("http://[::1]:1234/bla?a=b#c", "http", "::1", 1234, "/bla", "a=b", "c"),
+            new ValidVector("file:///bla", "file", null, 0, "/bla", null, null),
+            new InvalidVector("xxx"),
+            new InvalidVector("http://abc"),
+            new InvalidVector("http://::1/"),
+            new InvalidVector("http://::1:1234/"),
+            new InvalidVector("http://[::1:1234/"),
+            new InvalidVector("file:/bla//a/b"),
+            new InvalidVector("file://bla"),
+            new InvalidVector("file:/bla")
+    };
+
+    private static final Constructor[] constructors = new Constructor[]{
+            URIBuilder::new,
+            uri -> new URIBuilder(new URI(uri)),
+            uri -> new URIBuilder(new URL(uri))
+    };
+
+    @Test
+    public void testValid() throws URISyntaxException, MalformedURLException {
+        for (final @NotNull Vector vector : vectors) {
+            final @NotNull ValidVector validVector;
+
+            if (!(vector instanceof ValidVector))
+                continue;
+
+            validVector = (ValidVector) vector;
+
+            for (final @NotNull Constructor constructor : constructors) {
+                final @NotNull URIBuilder builder;
+
+                try {
+                    builder = constructor.construct(vector.uri);
+                } catch (Throwable e) {
+                    System.out.println("vector.uri = " + vector.uri);
+                    throw e;
+                }
+
+                System.out.println("builder.toString() = " + builder.toString());
+
+                assertEquals(validVector.scheme, builder.getRawScheme());
+                assertEquals(validVector.hostname, builder.getRawHostname());
+                assertEquals(validVector.port, builder.getRawPort());
+                assertEquals(validVector.path, builder.getRawPath());
+                assertEquals(validVector.query, builder.getRawQuery());
+                assertEquals(validVector.fragment, builder.getRawFragment());
+            }
+        }
+    }
+
+    @Test
+    public void testInvalid() {
+        for (final @NotNull Vector vector : vectors) {
+            if (vector instanceof ValidVector)
+                continue;
+
+            for (final @NotNull Constructor constructor : constructors) {
+                boolean failed = false;
+
+                try {
+                    constructor.construct(vector.uri);
+                } catch (URISyntaxException | MalformedURLException e) {
+                    failed = true;
+                }
+
+                if (!failed)
+                    fail("Constructor did not not fail for: " + vector.uri);
+            }
+        }
+    }
+}
