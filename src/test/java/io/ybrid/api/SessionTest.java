5729b219a75b579419b026650be746f328ad6df2
Philipp Schafft <philipp.schafft@nacamar.de>
2021-03-10 07:17:00 +0000

    Feature: Added a simple test with a fully offline session

--- src/test/java/io/ybrid/api/SessionTest.java
+++ src/test/java/io/ybrid/api/SessionTest.java
@@ -24,13 +24,17 @@ package io.ybrid.api;
 
 import io.ybrid.api.session.Command;
 import io.ybrid.api.transaction.Request;
+import io.ybrid.api.transaction.Transaction;
 import io.ybrid.api.transport.ServiceTransportDescription;
+import io.ybrid.api.transport.ServiceURITransportDescription;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URI;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class SessionTest {
     @Test
@@ -54,5 +58,34 @@ public class SessionTest {
 
             session.close();
         }
+    }
+
+    @Test
+    public void offlineSession() throws IOException {
+        final @NotNull URI streamURI = URI.create("http://localhost:8000/");
+        final @NotNull MediaEndpoint mediaEndpoint = new MediaEndpoint(streamURI);
+        final @NotNull Session session;
+        final @NotNull Transaction transaction;
+        final @Nullable Throwable error;
+
+        mediaEndpoint.forceApiVersion(ApiVersion.PLAIN);
+
+        session = mediaEndpoint.createSession();
+        session.connect();
+
+        assertTrue(session.isConnected());
+
+        session.attachPlayer(transportDescription -> {
+            final @NotNull ServiceURITransportDescription transport = (ServiceURITransportDescription) transportDescription;
+            assertEquals(streamURI, transport.getURI());
+        });
+
+        transaction = session.createTransaction(Command.CONNECT_INITIAL_TRANSPORT.makeRequest());
+        transaction.run();
+        error = transaction.getError();
+        if (error != null)
+            error.printStackTrace();
+        assertNull(error);
+        session.close();
     }
 }
