ab4e53e8ef21c58b0d62c8d75cbf04fdff8360c5
Philipp Schafft <philipp.schafft@nacamar.de>
2021-03-15 12:12:55 +0000

    Feature: Added basic MediaType class

---
+++ src/main/java/io/ybrid/api/util/MediaType.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2021 nacamar GmbH - Ybrid®, a Hybrid Dynamic Live Audio Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.ybrid.api.util;
+
+import io.ybrid.api.util.QualityMap.QualityMap;
+import io.ybrid.api.util.QualityMap.Style;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * This class represents a Media Type (such as used by the HTTP Content-Type header).
+ */
+public final class MediaType implements Serializable {
+    private static final long serialVersionUID = -2955670942461535881L;
+
+    public static final @NotNull MediaType MEDIA_TYPE_ANY = new MediaType("*/*");
+
+    /**
+     * The {@link Style} used for {@link QualityMap}.
+     */
+    public static final @NotNull Style<MediaType> STYLE = value -> {
+        final @NotNull Set<MediaType> ret = new HashSet<>();
+        final @NotNull String v = value.toString();
+
+        if (value.hasParameters()) {
+            ret.add(new MediaType(v.substring(0, v.indexOf(';'))));
+        }
+
+        ret.add(new MediaType(v.substring(0, v.indexOf('/')) + "/*"));
+        ret.add(MEDIA_TYPE_ANY);
+
+        return ret.toArray(new MediaType[0]);
+    };
+
+    private final @NotNull String mediaType;
+
+    /**
+     * Main constructor.
+     * @param mediaType The string representing the Media Type.
+     */
+    @Contract(pure = true)
+    public MediaType(@NotNull String mediaType) {
+        this.mediaType = mediaType.trim();
+    }
+
+    /**
+     * Returns whether this Media Type has parameters.
+     * @return Whether this Media Type has parameters.
+     */
+    @Contract(pure = true)
+    public boolean hasParameters() {
+        return mediaType.contains(";");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MediaType mediaType1 = (MediaType) o;
+        return mediaType.equals(mediaType1.mediaType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(serialVersionUID, mediaType);
+    }
+
+    @Override
+    public String toString() {
+        return mediaType;
+    }
+}
