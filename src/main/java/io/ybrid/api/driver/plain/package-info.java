e978fd427d6febb9b1df5d481c1b7dbdb1e9c646
Philipp Schafft <philipp.schafft@nacamar.de>
2021-03-16 07:58:40 +0000

    Update: Documented and marked as internal io.ybrid.api.driver*

---
+++ src/main/java/io/ybrid/api/driver/plain/package-info.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2021 nacamar GmbH - Ybrid®, a Hybrid Dynamic Live Audio Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * This package hosts a simple "plain" session driver.
+ * That is a session with the {@link io.ybrid.api.MediaEndpoint}
+ * and/or the {@code baseURI} is pointing directly to the playback URI.
+ * This is internal API and must not be used from the outside.
+ */
+@ApiStatus.Internal
+package io.ybrid.api.driver.plain;
+
+import org.jetbrains.annotations.ApiStatus;
